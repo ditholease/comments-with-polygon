f8f41cd1dadca7f997b8c272fa45d73dfaa83ecc
ditholease <86945140+ditholease@users.noreply.github.com>
2023-03-22 13:02:15 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import { providers } from "ethers";
 import Comments from "../components/Comments";
 
 // Provider that will be used when no wallet is connected (aka no signer)
-const provider = providers.getDefaultProvider("https://roxycoin.eu.ngrok.io/");
+const provider = providers.getDefaultProvider("https://rpc.roxycoin.be");
 //const provider = new ethers.providers.Web3Provider(connection);
 
 // Create a react-query client
